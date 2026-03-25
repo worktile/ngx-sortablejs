8d0c975a9c13cf59bc939907d0f22210e6cda91d
xujing0521 <1332570609@qq.com>
2026-03-25 16:12:15 +0800

    build: bump angular v21

--- src/app/examples/simple-sortable/simple-sortable.component.ts
+++ src/app/examples/simple-sortable/simple-sortable.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgFor, JsonPipe } from '@angular/common';
+import { JsonPipe } from '@angular/common';
 import { SortablejsDirective } from '../../../../projects/ngx-sortablejs/src/lib/sortablejs.directive';
 
 @Component({
@@ -7,10 +7,9 @@ import { SortablejsDirective } from '../../../../projects/ngx-sortablejs/src/lib
     templateUrl: './simple-sortable.component.html',
     styleUrls: ['./simple-sortable.component.css'],
     imports: [
-        SortablejsDirective,
-        NgFor,
-        JsonPipe,
-    ]
+    SortablejsDirective,
+    JsonPipe
+]
 })
 export class SimpleSortableComponent {
 
--- src/app/test-cases/cross-components-multiple-lists/child-component/child-component.component.ts
+++ src/app/test-cases/cross-components-multiple-lists/child-component/child-component.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 import {Options} from 'sortablejs';
-import { NgFor } from '@angular/common';
+
 import { SortablejsDirective } from '../../../../../projects/ngx-sortablejs/src/lib/sortablejs.directive';
 
 @Component({
     selector: 'app-child-component',
     templateUrl: './child-component.component.html',
     styleUrls: ['./child-component.component.css'],
-    imports: [SortablejsDirective, NgFor]
+    imports: [SortablejsDirective]
 })
 export class ChildComponentComponent {
 
